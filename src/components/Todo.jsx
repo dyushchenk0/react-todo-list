7f6fed7805854b8fd1f186e0bf17f3a8b80ec261
Danile Yushchenko <dyushchenko0@gmail.com>
2026-01-20 16:06:50 +0200

    refactor(ui): simplify components by using props for dynamic rendering

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,13 +4,21 @@ import TodoInfo from "@/components/TodoInfo.jsx";
 import TodoList from "@/components/TodoList.jsx";
 
 const Todo = () => {
+  const tasks = [
+    { id: 'task-1', title: 'buy milk', isDone: false},
+    { id: 'task-2', title: 'Rest', isDone: true},
+    { id: 'task-3', title: 'Set home', isDone: true},
+  ]
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
       <AddTaskForm />
       <SearchTaskForm />
-      <TodoInfo />
-      <TodoList />
+      <TodoInfo
+        total={tasks.length}
+        done={tasks.filter(({ isDone }) => isDone).length}
+      />
+      <TodoList tasks={tasks}/>
     </div>
   )
 }
